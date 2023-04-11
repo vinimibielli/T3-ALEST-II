3bc71fb45ba4c3a4dd7a02675529da59193106e2
Lucas Damo <lucasmottadamo@gmail.com>
2023-04-11 19:59:16 -0300

    fixed weird code on main

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 
-#include "Empresa.h"
-#include "EmpresaService.h"
-#include "EmpresaController.h"
-#include "Descontos.h"
-#include "Produtos.h"
-#include "Compras.h"
-#include "Application.h"
+
 
 int main() {
-    Application *app = new Application();
-    app->init();
-    std::cout<<"main"<<std::endl;
-    cout<<app->criaListaCompras();
+    std::cout<< "Template-Ic"<<std::endl;
 }
